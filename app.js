879366c46ad3c3e8f0e0daa750394ad88d079854
jorgedq <jorgedq7952@gmail.com>
2020-08-24 21:50:28 -0400

    header

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-  clickHamburguer();
-});
+document.addEventListener('DOMContentLoaded', e => {
+  clickHamburguer()
+})
+
+window.addEventListener('resize', e => {
+  changeClasesResize(e)
+})
+
+function changeClasesResize(event) {
+  if (event.currentTarget.innerWidth < 992) {
+    document.getElementById('menuID').classList.add('menu')
+  } else {
+    if (event.currentTarget.innerWidth >= 992) {
+      closeMenu(document.getElementById('hamburger'))
+      document.getElementById('menuID').classList.remove('menu', 'menu--active')
+    }
+  }
+}
 
 function clickHamburguer() {
-  const $hamburger = document.getElementById("hamburger"),
-    $menu = document.getElementById("menu");
-  $hamburger.addEventListener("click", (e) => {
-    if ($hamburger.dataset.on === "false") {
-      openMenu($hamburger);
-      toggleMenu($menu);
+  const $hamburger = document.getElementById('hamburger'),
+    $menu = document.getElementById('menuID')
+  $hamburger.addEventListener('click', e => {
+    if ($hamburger.dataset.on === 'false') {
+      openMenu($hamburger)
+      toggleMenu($menu)
     } else {
-      closeMenu($hamburger);
-      toggleMenu($menu);
+      closeMenu($hamburger)
+      toggleMenu($menu)
     }
-  });
+  })
 }
-// open menu function
+
 function openMenu($hamburger) {
-  $hamburger.setAttribute("data-on", "true");
-  $hamburger.setAttribute("src", "./images/icon-close.svg");
+  $hamburger.setAttribute('data-on', 'true')
+  $hamburger.setAttribute('src', './images/icon-close.svg')
 }
-// close menu function
+
 function closeMenu($hamburger) {
-  $hamburger.setAttribute("data-on", "false");
-  $hamburger.setAttribute("src", "./images/icon-hamburger.svg");
+  $hamburger.setAttribute('data-on', 'false')
+  $hamburger.setAttribute('src', './images/icon-hamburger.svg')
 }
-// toggle menu function
+
 function toggleMenu($menu) {
-  $menu.classList.toggle("d-none");
+  $menu.classList.toggle('menu--active')
 }
